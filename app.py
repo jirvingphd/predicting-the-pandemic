d1fc423a575b6c59d371f94ae176ad0a703ad221
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2021-09-08 21:52:37 -0400

    update app

--- app.py
+++ app.py
@@ -1,1 +1,78 @@
 import streamlit as st
+
+from fsds.imports import *
+
+import os,glob,sys,joblib,zipfile,json
+import re
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates.default = "plotly_dark"
+
+plt.rcParams['figure.figsize'] = (12,6)
+pd.set_option('display.max_columns',0)
+# fs.check_package_versions(['statsmodels'],fpath=True)
+RUN_FULL_WORKFLOW=False
+
+
+
+### FORECAST SPECIFIC FUNCTIONS
+import statsmodels.api as sms
+import statsmodels.tsa.api as tsa
+from pmdarima import auto_arima
+import project_functions as fn
+import os,json,glob
+
+with open("FILE_DIRECTORY.json") as f:
+    FPATHS = json.load(f)
+
+
+
+if RUN_FULL_WORKFLOW:
+    df_states,STATES = fn.data_acquisition.FULL_WORKFLOW(merge_hospital_data=True)
+    ## renaming since merge_hofspital_data=True
+#     DF = df_states.copy()
+#     print(STATES.keys())    
+    
+else:
+    print(f"[i] Using previously downloaded data...")
+    df_states = pd.read_pickle(FPATHS['fpath_final_df_pickle'])
+    
+#     with open(FPATHS['fpath_final_states']) as f:
+    STATES = joblib.load(FPATHS['fpath_final_states'])
+#     df_states = pd.read_csv(os.path.join(fpath_clean,
+#                                          'combined_us_states_full_data.csv'))
+#     STATES = joblib.load(os.path.join(fpath_clean,'STATE_DICT.joblib'))
+
+
+
+st.write('Planning for the Pandemic')
+
+
+state_name = st.sidebar.selectbox('Select State', list(STATES.keys()))
+col = STATES["NY"].columns.to_list()
+df_state = STATES[state_name].copy()
+
+# col = 'Cases-New'
+ts = df_state[col].copy()
+ax = ts.plot(title=f"{state_name}-{col}");
+ax.set_ylabel(col)
+plt.show()
+
+
+
+
+# st.button('Hit me')
+# st.checkbox('Check me out')
+# st.radio('Radio', [1,2,3])
+# st.multiselect('Multiselect', [1,2,3])
+# st.slider('Slide me', min_value=0, max_value=10)
+# st.select_slider('Slide to select', options=[1,'2'])
+# st.text_input('Enter some text')
+# st.number_input('Enter a number')
+# st.text_area('Area for textual entry')
+# st.date_input('Date input')
+# st.time_input('Time entry')
+# st.file_uploader('File uploader')
+# st.color_picker('Pick a color')
